145b1c795fb25c94eb71a20824d2631d18e9f2f0
ottokah <otto.kahilainen@gmail.com>
2022-04-28 14:57:11 +0300

    New activcity

--- app/src/main/java/fi/jimihautamaki/delta/MainActivity.java
+++ app/src/main/java/fi/jimihautamaki/delta/MainActivity.java
@@ -2,13 +2,29 @@ package fi.jimihautamaki.delta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button kehitys;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        kehitys = (Button) findViewById(R.id.kehitys);
+        kehitys.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openOmaKehitys();
+            }
+        });
+    }
+    public void openOmaKehitys() {
+        Intent intent = new Intent(this, OmaKehitys.class);
+        startActivity(intent);
     }
 }
