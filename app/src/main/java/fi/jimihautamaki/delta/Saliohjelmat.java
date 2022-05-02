2cf359b3370e576d0312ab249fbbbc47b9f27b26
oliv3r1 <oliver.suomela@metropolia.fi>
2022-05-02 18:33:33 +0300

    Saliohjelmat sivulle täytettä

--- app/src/main/java/fi/jimihautamaki/delta/Saliohjelmat.java
+++ app/src/main/java/fi/jimihautamaki/delta/Saliohjelmat.java
@@ -12,6 +12,9 @@ import android.widget.TextView;
 public class Saliohjelmat extends AppCompatActivity {
     private Button infoButton;
     private TextView infoTextView;
+    private Button rintaOlka;
+    private  Button selkaHauis;
+    private Button jalat;
 
 
     @Override
@@ -20,7 +23,77 @@ public class Saliohjelmat extends AppCompatActivity {
         setContentView(R.layout.activity_saliohjelmat);
 
         infoButton = (Button) findViewById(R.id.Info_Button);
+        rintaOlka = (Button) findViewById(R.id.rinta);
+        selkaHauis = (Button) findViewById(R.id.selka);
+        jalat = (Button) findViewById(R.id.jalkapaiva);
         infoTextView = (TextView) findViewById(R.id.InfoTextView);
+
+        jalat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Saliohjelmat.this);
+
+                builder.setCancelable(true);
+                builder.setTitle("Jalkatreeni");
+                builder.setMessage("Kyykky tangolla 4x10\nJalkaprässi 4x10\nReiden ojennus 3x10\nReiden loitonnus 3x10\n" +
+                        "Bulgarialainen kyykky 3x10\nPohkeet 4x12\nIstumaannousu 3x20\nLankutus 1xMax");
+
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        infoTextView.setVisibility(View.VISIBLE);
+                    }
+                });
+
+                builder.show();
+            }
+        });
+
+        selkaHauis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Saliohjelmat.this);
+
+                builder.setCancelable(true);
+                builder.setTitle("Selkä ja Hauis");
+                builder.setMessage("Ylätalja 4x8\nKulmasoutu tangolla 3x10\nAlatalja 4x8\nLeuanveto\n" +
+                        "Hauiskääntö käsipainoilla 3x10\nHauiskääntö taljassa 3x10\nHauiskääntö EZ tangolla 3x10");
+
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        infoTextView.setVisibility(View.VISIBLE);
+                    }
+                });
+
+                builder.show();
+            }
+        });
+
+        rintaOlka.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Saliohjelmat.this);
+
+                builder.setCancelable(true);
+                builder.setTitle("Rinta/Olkapää/Ojentaja");
+                builder.setMessage("Penkkipunnerrus 4x8\nVinopenkki käsipainoilla 3x8\nPec deck 3x10\n" +
+                        "Pystypunnerrus tangolla 3x8\nVipunostot eteen 3x10\nVipunostot sivuille 3x10\nOjentaja punnerrus taljassa 4x8\n" +
+                        "Ojentajapunnerrus EZ tangolla 3x10");
+
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        infoTextView.setVisibility(View.VISIBLE);
+                    }
+                });
+
+                builder.show();
+            }
+        });
 
         infoButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,8 +101,8 @@ public class Saliohjelmat extends AppCompatActivity {
                 AlertDialog.Builder builder = new AlertDialog.Builder(Saliohjelmat.this);
 
                 builder.setCancelable(true);
-                builder.setTitle("Tarvitsetko apua?");
-                builder.setMessage("Saliohjelmat: Nappia painamalla voit valita listasta mieleisen treeniohjelman \n\nOma kehitys: Kuvajainen näyttää sinulle viimeaikaisen kehityksen harjoitteluistasi.\n\n Omat treenit: Tänne voit täyttää treenisiisi liittyvät tiedot ja painot millä olet harjoitellut");
+                builder.setTitle("Saliohjelmat");
+                builder.setMessage("Tältä sivulta voit tarkastella saliohjelmia ja valita itsellesi mielestäsi sopivimman.");
 
 
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
