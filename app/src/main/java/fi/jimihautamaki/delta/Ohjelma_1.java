afcdcab67689c201093b8671ef7c403d3733e8b7
ottokah <otto.kahilainen@gmail.com>
2022-05-05 17:25:06 +0300

    Kehitys

--- app/src/main/java/fi/jimihautamaki/delta/Ohjelma_1.java
+++ app/src/main/java/fi/jimihautamaki/delta/Ohjelma_1.java
@@ -2,6 +2,7 @@ package fi.jimihautamaki.delta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class Ohjelma_1 extends AppCompatActivity {
@@ -12,5 +13,15 @@ public class Ohjelma_1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ohjelma1);
+    }
+
+
+    String fileNameString = sharedPreferencesBinding.fileNameEditView.getText().toString();
+    SharedPreferences sharedPreferences;
+if(fileNameString.isEmpty()) {
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+    }
+else {
+        sharedPreferences = getSharedPreferences(fileNameString, MODE_PRIVATE);
     }
 }
