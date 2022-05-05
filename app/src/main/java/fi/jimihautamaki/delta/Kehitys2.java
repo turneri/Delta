58bde75b6d5fce786ed85254e9fa1a8ce3346fb7
ottokah <otto.kahilainen@gmail.com>
2022-05-05 11:58:48 +0300

    Layout

--- app/src/main/java/fi/jimihautamaki/delta/Kehitys2.java
+++ app/src/main/java/fi/jimihautamaki/delta/Kehitys2.java
@@ -1,4 +1,15 @@
 package fi.jimihautamaki.delta;
 
-public class Kehitys2 {
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class Kehitys2 extends AppCompatActivity {
 }
